6491bb112491e912d3e1390a196edceaeab52dde
aksoni <aksoni@wisc.edu>
2016-03-12 17:31:19 -0600

    Added calendar view and add/view activities

--- app/src/main/java/com/example/abhi/calendar/MainActivity.java
+++ app/src/main/java/com/example/abhi/calendar/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.abhi.calendar;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+
+   /* protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }*/
+
+
+    private int selectedYear;
+    private int selectedMonth;
+    private int selectedDay;
+    CalendarView calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //sets the main layout of the activity
         setContentView(R.layout.activity_main);
+
+        //initializes the calendarview
+        initializeCalendar();
     }
+
+    public void initializeCalendar() {
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        //sets the listener to be notified upon selected date change.
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //show the selected date as a toast
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                selectedYear = year;
+                selectedMonth = month;
+                selectedDay = day;
+            }
+        });
+    }
+
+    public void onClickAdd (View view)
+    {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickView (View view)
+    {
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
+    }
+
+
 }
